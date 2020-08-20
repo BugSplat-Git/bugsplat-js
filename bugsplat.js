35f82f892225f7d09b06c04b631c71ba0bed97b8
Bobby Galli <bobbyg603@gmail.com>
2020-08-20 17:29:26 -0400

    Updates to tease out node.js specifics so this can run in browsers.

--- bugsplat.js
+++ bugsplat.js
@@ -1,8 +1,5 @@
-const fetch = require("node-fetch");
-const formData = require("form-data");
-const fs = require("fs");
-const path = require("path");
-
+const fetch = globalThis.fetch ? globalThis.fetch : require("node-fetch");
+const formData = globalThis.FormData ? globalThis.FormData : require("form-data");
 
 
 module.exports = function (database, appName, appVersion) {
@@ -22,17 +19,13 @@ module.exports = function (database, appName, appVersion) {
     this._appName = appName;
     this._appVersion = appVersion;
 
-    this._additionalFilePaths = [];
+    this._additionalFormDataParams = [];
     this._appKey = '';
     this._description = '';
     this._email = '';
     this._user = '';
     this._formData = formData;
     this._fetch = fetch;
-
-    this.setDefaultAdditionalFilePaths = (additionalFilePaths) => {
-        this._additionalFilePaths = additionalFilePaths;
-    }
 
     this.setDefaultAppKey = (appKey) => {
         this._appKey = appKey;
@@ -57,7 +50,7 @@ module.exports = function (database, appName, appVersion) {
         const user = options.user || this._user;
         const email = options.email || this._email;
         const description = options.description || this._description;
-        const additionalFilePaths = options.additionalFilePaths || this._additionalFilePaths;
+        const additionalFormDataParams = options.additionalFormDataParams || [];
 
         const url = "https://" + database + ".bugsplat.com/post/js/";
         const callstack = !errorToPost.stack ? errorToPost : errorToPost.stack;
@@ -71,7 +64,9 @@ module.exports = function (database, appName, appVersion) {
         body.append("email", email);
         body.append("description", description);
         body.append("callstack", callstack);
-        this._addAdditionalFilesToBody(body, additionalFilePaths);
+        additionalFormDataParams.forEach(param => body.append(param.key, param.value));
+        // TODO move to bugsplat-node
+        //this._addAdditionalFilesToBody(body, additionalFilePaths);
 
         console.log("BugSplat Error:", errorToPost);
         console.log("BugSplat Url:", url);
@@ -95,31 +90,6 @@ module.exports = function (database, appName, appVersion) {
         }
 
         return this._createReturnValue(null, json, errorToPost);
-    }
-
-    this.postAndExit = async (errorToPost, options) => {
-        return this.post(errorToPost, options).then(() => process.exit(1));
-    }
-
-    this._addAdditionalFilesToBody = (body, additionalFilePaths) => {
-        let totalZipSize = 0;
-        for (var i = 0; i < additionalFilePaths.length; i++) {
-            const filePath = additionalFilePaths[i];
-            if (fs.existsSync(filePath)) {
-                const fileSize = fs.statSync(filePath).size;
-                totalZipSize = totalZipSize + fileSize;
-                if (totalZipSize <= 1048576) {
-                    const fileName = path.basename(filePath);
-                    const fileContents = fs.createReadStream(filePath);
-                    body.append(fileName, fileContents);
-                } else {
-                    console.error("BugSplat upload limit of 1MB exceeded, skipping file:", filePath);
-                    totalZipSize = totalZipSize - fileSize;
-                }
-            } else {
-                console.error("BugSplat file doesn't exist at path:", filePath);
-            }
-        }
     }
 
     this._createReturnValue = (error, response, original) => {
