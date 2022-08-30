1357a29345c89585a7ae901d9904107e7b6b3d71
Bobby Galli <bobbyg603@gmail.com>
2022-08-30 21:38:18 +0000

    fix: export BugSplatResponseBody and BugSplatResponseType (#62)
    * fix: export BugSplatResponseBody and BugSplatResponseType
    
    * chore: fix broken test

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { BugSplat } from './bugsplat';
 export type { BugSplatOptions } from './bugsplat-options';
-export type { BugSplatResponse } from './bugsplat-response';
+export type { BugSplatResponse, BugSplatResponseBody, BugSplatResponseType } from './bugsplat-response';
 export type { FormDataParam } from './form-data-param';
